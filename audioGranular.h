598f62184a5ec2c67ed0e308fd57a2b124467889
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-05-29 21:03:09 +0200

    remove audio file

--- audioGranular.h
+++ audioGranular.h
@@ -1,28 +1,83 @@
 #ifndef _AUDIO_GRANULAR_H_
 #define _AUDIO_GRANULAR_H_
 
-#include "audioFile.h"
+#include <sndfile.h>
+
 #include "def.h"
 
+#define AUDIO_BUFFER_SECONDS 30
+#define LOAD_SAMPLE_IN_MEMORY 1
+#define AUDIO_BUFFER_SIZE SAMPLE_RATE* AUDIO_BUFFER_SECONDS
+
 class AudioGranular {
+protected:
+    int samplePos = 0;
+    float buffer[AUDIO_BUFFER_SIZE];
+    bool isOpen = false;
+
 public:
-    AudioFile audioFile;
+    SF_INFO sfinfo;
+    SNDFILE* file = NULL;
 
     AudioGranular()
     {
-        // audioFile.open("samples/0_squeaky.wav");
-        // audioFile.open("samples/0_fx_01.wav");
-        audioFile.open("samples/0_axxe13.wav");
+        memset(&sfinfo, 0, sizeof(sfinfo));
+
+        // open("samples/0_squeaky.wav");
+        // open("samples/0_fx_01.wav");
+        open("samples/0_axxe13.wav");
     }
 
-    void noteOn()
+    ~AudioGranular()
     {
-        audioFile.restart();
+        close();
+    }
+
+    AudioGranular& close()
+    {
+        isOpen = false;
+        if (file) {
+            sf_close(file);
+        }
+        return *this;
+    }
+
+    // By default, move to the end of the sample to avoid unwanted playback.
+    AudioGranular& open(const char* filename, int seek = SEEK_END)
+    {
+        close();
+
+        if (!(file = sf_open(filename, SFM_READ, &sfinfo))) {
+            printf("Error: could not open file %s\n", filename);
+            return *this;
+        }
+        printf("Audio file %s sampleCount %ld sampleRate %d\n", filename, (long)sfinfo.frames, sfinfo.samplerate);
+        isOpen = true;
+
+        sf_read_float(file, buffer, AUDIO_BUFFER_SIZE);
+        samplePos = sfinfo.frames;
+
+        return *this;
     }
 
     int64_t samples(float* buf, int len)
     {
-        return audioFile.samples(buf, len);
+        int i = 0;
+        for (; i < len; i++) {
+            if (samplePos < sfinfo.frames) {
+                buf[i] = buffer[samplePos];
+                samplePos++;
+            } else {
+                buf[i] = 0;
+            }
+        }
+        return i;
+    }
+
+    AudioGranular& noteOn()
+    {
+        samplePos = 0;
+        return *this;
     }
 };
 
