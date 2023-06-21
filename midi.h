e967cc98dfecfd04eebe20b576477fd611c2ff9d
Alexandre Piel <alexandre.piel@gmail.com>
2023-06-21 20:18:18 +0200

    voice

--- midi.h
+++ midi.h
@@ -86,18 +86,18 @@
 
 void midiKeyboardCallback(double deltatime, std::vector<unsigned char>* message, void* userData)
 {
-    // if (message->at(0) == 0x90) {
-    //     AudioHandler::get().audioGranular.noteOn(message->at(1), message->at(2));
-    // } else if (message->at(0) == 0x80) {
-    //     AudioHandler::get().audioGranular.noteOff(message->at(1), message->at(2));
-    // } else {
+    if (message->at(0) == 0x90) {
+        AudioHandler::get().audioGranular.noteOn(message->at(1), message->at(2));
+    } else if (message->at(0) == 0x80) {
+        AudioHandler::get().audioGranular.noteOff(message->at(1), message->at(2));
+    } else {
         printf("Midi keyboard message: ");
         unsigned int nBytes = message->size();
         for (unsigned int i = 0; i < nBytes; i++) {
             printf("%02x ", (int)message->at(i));
         }
         printf("\n");
-    // }
+    }
 }
 
 void midiControllerCallback(double deltatime, std::vector<unsigned char>* message, void* userData)
