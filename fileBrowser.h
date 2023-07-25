34559aab0a65a44a5b5af4d42025b457d5ea8ea9
Alexandre Piel <alexandre.piel@zooplus.com>
2023-07-25 22:23:11 +0200

    fix

--- fileBrowser.h
+++ fileBrowser.h
@@ -4,7 +4,7 @@
 #include "def.h"
 #include <dirent.h>
 
-#define FILE_BROWSER_FILES_MAX 40
+#define FILE_BROWSER_FILES_MAX 127
 #define FILE_BROWSER_FILEPATH_LEN 258
 #define FILE_BROWSER_FILENAME_LEN 255
 
@@ -35,8 +35,8 @@ protected:
     {
         uint8_t i, j;
         char tmp[FILE_BROWSER_FILENAME_LEN];
-        for (i = 0; i < FILE_BROWSER_FILES_MAX - 1; i++) {
-            for (j = i + 1; j < FILE_BROWSER_FILES_MAX; j++) {
+        for (i = 0; i < count - 1; i++) {
+            for (j = i + 1; j < count; j++) {
                 if (strcmp(files[i], files[j]) > 0) {
                     strncpy(tmp, files[i], FILE_BROWSER_FILENAME_LEN);
                     strncpy(files[i], files[j], FILE_BROWSER_FILENAME_LEN);
@@ -45,13 +45,6 @@ protected:
             }
         }
     }
-
-    // void dump()
-    // {
-    //     for (uint8_t i = 0; i < FILE_BROWSER_FILES_MAX; i++) {
-    //         printf("%d. %s\n", i, files[i]);
-    //     }
-    // }
 
 public:
     uint8_t position = 0;
@@ -68,7 +61,7 @@ public:
 
     char* getFile(uint8_t pos)
     {
-        position = range(pos, 0, FILE_BROWSER_FILES_MAX - 1);
+        position = range(pos, 0, count - 1);
         snprintf(file, FILE_BROWSER_FILEPATH_LEN, "%s/%s", folder, files[position]);
         return file;
     }
