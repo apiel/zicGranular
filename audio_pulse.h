8073262e673e65add81b3fbab10191e2c5a456c1
Alexandre Piel <alexandre.piel@zooplus.com>
2023-07-27 19:42:46 +0200

    pulse

--- audio_pulse.h
+++ audio_pulse.h
@@ -1,20 +1,17 @@
 #ifndef _AUDIO_P_H_
 #define _AUDIO_P_H_
 
+#include <pulse/simple.h>
+
 #include "audioHandler.h"
 #include "def.h"
 
 AudioHandler& audioHandlerPulse = AudioHandler::get();
 
-// int audioCallback(void* outputBuffer, void* /*inputBuffer*/, unsigned int nBufferFrames,
-//     double /*streamTime*/, RtAudioStreamStatus status, void* data)
-// {
-//     audioHandler.samples((float*)outputBuffer, nBufferFrames * APP_CHANNELS);
-//     return 0;
-// }
-#include <pulse/simple.h>
 
-int openpulse() {
+int openpulse()
+{
+
     static const pa_sample_spec streamFormat = {
         .format = PA_SAMPLE_FLOAT32LE,
         // .format = PA_SAMPLE_FLOAT32BE,
@@ -22,14 +19,16 @@ int openpulse() {
         .channels = APP_CHANNELS,
     };
 
-    pa_simple *device = pa_simple_new(NULL, NULL, PA_STREAM_PLAYBACK, NULL, 
-            "playback", &streamFormat, NULL, NULL, NULL);
+    pa_simple* device = pa_simple_new(NULL, NULL, PA_STREAM_PLAYBACK, NULL,
+        "playback", &streamFormat, NULL, NULL, NULL);
+
+    uint8_t bufferMultiplier = sizeof(float) / sizeof(uint8_t);
 
     while (1) {
-        float outputBuffer[1024];
-        audioHandlerPulse.samples((float*)outputBuffer, 256);
+        float outputBuffer[APP_AUDIO_CHUNK];
+        audioHandlerPulse.samples((float*)outputBuffer, APP_AUDIO_CHUNK);
 
-        pa_simple_write(device, outputBuffer, 1024, NULL);
+        pa_simple_write(device, outputBuffer, APP_AUDIO_CHUNK * bufferMultiplier, NULL);
     }
 
     pa_simple_drain(device, NULL);
