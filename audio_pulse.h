375699a2dd4c3474b30cb67cbea32c32801cc6e7
Alexandre Piel <alexandre.piel@zooplus.com>
2023-07-27 21:13:47 +0200

    use audio output from config

--- audio_pulse.h
+++ audio_pulse.h
@@ -7,12 +7,7 @@
 #include "audioHandler.h"
 #include "def.h"
 
-static void rt_pa_set_server_info(pa_context* context, const pa_server_info* info, void* userdata)
-{
-    printf("default sink name: %s\n", info->default_sink_name);
-}
-
-static void rt_pa_set_sink_info(pa_context* c, const pa_sink_info* i,
+static void pa_set_sink_info(pa_context* c, const pa_sink_info* i,
     int eol, void* userdata)
 {
     if (eol) {
@@ -21,12 +16,11 @@ static void rt_pa_set_sink_info(pa_context* c, const pa_sink_info* i,
         return;
     }
 
-    const char* name = pa_proplist_gets(i->proplist, "device.description");
-    // printf("name: %s (outputChannels %d preferredSampleRate %d)\n", name, i->sample_spec.channels, i->sample_spec.rate);
-    printf("- %s\n", name);
+    // printf("name: %s (outputChannels %d preferredSampleRate %d)\n", pa_proplist_gets(i->proplist, "device.description"), i->sample_spec.channels, i->sample_spec.rate);
+    APP_PRINT("- %s [AUDIO_OUTPUT=%s]\n", pa_proplist_gets(i->proplist, "device.description"), i->name);
 }
 
-static void rt_pa_context_state_callback(pa_context* context, void* userdata)
+static void pa_context_state_callback(pa_context* context, void* userdata)
 {
     pa_mainloop_api* mlApi = (pa_mainloop_api*)userdata;
 
@@ -37,10 +31,9 @@ static void rt_pa_context_state_callback(pa_context* context, void* userdata)
         break;
 
     case PA_CONTEXT_READY:
-        printf("PA_CONTEXT_READY\n");
-        pa_context_get_server_info(context, rt_pa_set_server_info, userdata); // server info
-        pa_context_get_sink_info_list(context, rt_pa_set_sink_info, userdata); // output info ... needs to be before input
-        // pa_context_get_source_info_list(context, rt_pa_set_source_info_and_quit, userdata); // input info
+        debug("PA_CONTEXT_READY\n");
+        pa_context_get_sink_info_list(context, pa_set_sink_info, userdata); // output info ... needs to be before input
+        // pa_context_get_source_info_list(context, pa_set_source_info_and_quit, userdata); // input info
         break;
 
     case PA_CONTEXT_TERMINATED:
@@ -49,7 +42,7 @@ static void rt_pa_context_state_callback(pa_context* context, void* userdata)
 
     case PA_CONTEXT_FAILED:
     default:
-        printf("PA_CONTEXT_FAILED\n");
+        debug("PA_CONTEXT_FAILED\n");
         mlApi->quit(mlApi, 0);
     }
 }
@@ -78,27 +71,27 @@ protected:
     void listDevices()
     {
         if (!(ml = pa_mainloop_new())) {
-            printf("pa_mainloop_new() failed.\n");
+            debug("pa_mainloop_new() failed.\n");
             return freeListDevice();
         }
 
         pa_mainloop_api* paMainLoopApi = pa_mainloop_get_api(ml);
 
         if (!(context = pa_context_new_with_proplist(paMainLoopApi, NULL, NULL))) {
-            printf("pa_context_new() failed.\n");
+            debug("pa_context_new() failed.\n");
             return freeListDevice();
         }
 
-        pa_context_set_state_callback(context, rt_pa_context_state_callback, paMainLoopApi);
+        pa_context_set_state_callback(context, pa_context_state_callback, paMainLoopApi);
 
         if (pa_context_connect(context, server, PA_CONTEXT_NOFLAGS, NULL) < 0) {
-            printf("pa_context_connect() failed: %s\n", pa_strerror(pa_context_errno(context)));
+            debug("pa_context_connect() failed: %s\n", pa_strerror(pa_context_errno(context)));
             return freeListDevice();
         }
 
         int ret = 1;
         if (pa_mainloop_run(ml, &ret) < 0) {
-            printf("pa_mainloop_run() failed.\n");
+            debug("pa_mainloop_run() failed.\n");
         }
         freeListDevice();
     }
@@ -116,13 +109,11 @@ public:
     {
         static const pa_sample_spec streamFormat = {
             .format = PA_SAMPLE_FLOAT32LE,
-            // .format = PA_SAMPLE_FLOAT32BE,
             .rate = SAMPLE_RATE,
             .channels = APP_CHANNELS,
         };
 
-        pa_simple* device = pa_simple_new(NULL, NULL, PA_STREAM_PLAYBACK, NULL,
-            "zicGranular", &streamFormat, NULL, NULL, NULL);
+        pa_simple* device = pa_simple_new(NULL, NULL, PA_STREAM_PLAYBACK, audioOutput, "zicGranular", &streamFormat, NULL, NULL, NULL);
 
         uint8_t bufferMultiplier = sizeof(float) / sizeof(uint8_t);
 
